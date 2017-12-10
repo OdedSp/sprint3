03d99f18552377d5281b5698a40bf9e359a5d289
Oded Spitzer <odedspitzer@gmail.com>
2017-12-10 14:51:36 +0200

    fully functional and cleaner

--- pages/MyMail.js
+++ pages/MyMail.js
@@ -15,20 +15,23 @@ export default {
         
         <input type="text" v-model="searchTerm" @keyup="changeView">
 
-        <div class="radioDisplay" v-show="show==='inbox'">
-        <input v-model="display" type="radio" id="all"
-        name="display" value="all" checked>
+        <div class="statusDisplay" v-show="show==='inbox'">
+        <input v-model="statusDisplay" type="radio" id="all"
+        name="statusDisplay" value="all" checked>
         <label for="all">All</label>
         
-        <input type="radio" id="read"
-        name="display" value="read">
+        <input v-model="statusDisplay" type="radio" id="read"
+        name="statusDisplay" value="read">
         <label for="read">Read</label>
         
-        <input type="radio" id="unread"
-        name="display" value="unread">
+        <input v-model="statusDisplay" type="radio" id="unread"
+        name="statusDisplay" value="unread">
         <label for="unread">Unread</label>
         </div>
-        <msgs-box :msgs="msgs" :show="show" :searchTerm="searchTerm" @msgCliked="routeToMsg" @deleteMsg="deleteMsg" @markUnread="markUnread">
+
+        {{statusDisplay}}
+        <msgs-box :msgs="msgs" :show="show" :searchTerm="searchTerm" :statusDisplay="statusDisplay"
+        @msgCliked="routeToMsg" @deleteMsg="deleteMsg" @markUnread="markUnread">
         </msgs-box>
         <show-msg :selectedMsg="selectedMsg" v-if="selectedMsg"
         @deleteMsg="deleteMsg" @sendMsg="sendMsg" @saveDraft="saveDraft" @markUnread="markUnread">
@@ -42,7 +45,7 @@ export default {
             compose: false,
             msgs: [],
             show: 'inbox',
-            display: 'all',
+            statusDisplay: 'all',
             searchTerm: '',
             selectedMsg: null,
         }
